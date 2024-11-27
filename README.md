481ad606ee55d5c5e3fd5f53e726766f5c5caf6b
sakshishukla013 <sakshishukla013@gmail.com>
2024-11-27 11:40:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NotesKeeper
+A WebApp made using react.js for keeping all of your important notes/tasks safe.
+![Screenshot 2022-12-22 072146](https://user-images.githubusercontent.com/86218655/209037887-11815829-4af1-4323-aa0f-c99a633e4182.png)
+Deployed:https://vaibhavdwivedi-321.github.io/NotesKeeper/
---
+++ build/precache-manifest.bbe817dedaed249b83b396d2e02a75f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5106a9c1e90df5a24078133d34283c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8bbe94d9aab3969a360",
+    "url": "/static/js/2.6ee16f53.chunk.js"
+  },
+  {
+    "revision": "d53d9a98cf78c242286c",
+    "url": "/static/js/main.39176da4.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
